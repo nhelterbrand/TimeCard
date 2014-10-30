f6ff72cf0aac8494f70de45f7d9833117bb5f304
bsknowl <bsknowlton@ucdavis.edu>
2014-10-30 13:12:15 -0700

    added TimeCardView Controller and storyboard

---
+++ TimeCard/Classes/TimeCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimeCardViewController.h
+//  TimeCard
+//
+//  Created by BRIAN KNOWLTON on 10/30/14.
+//  Copyright (c) 2014 Etherios. All rights reserved.
+//
+
+#ifndef TimeCard_TimeCardViewController_h
+#define TimeCard_TimeCardViewController_h
+
+
+#endif
---
+++ TimeCard/Classes/TimeCardViewController.m
@@ -0,0 +1,9 @@
+//
+//  TimeCardViewController.m
+//  TimeCard
+//
+//  Created by BRIAN KNOWLTON on 10/30/14.
+//  Copyright (c) 2014 Etherios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
